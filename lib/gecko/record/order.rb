e0f9aabeb708187e146593132cd3221c9e0a8714
Bradley Priest <bradleypriest@gmail.com>
2014-03-05 21:33:44 +1300

    Set class_name for Order addresses

--- lib/gecko/record/order.rb
+++ lib/gecko/record/order.rb
@@ -7,8 +7,8 @@ module Gecko
       has_many :invoices
       has_many :order_line_items
       belongs_to :company
-      belongs_to :shipping_address
-      belongs_to :billing_address
+      belongs_to :shipping_address,     class_name: "Address"
+      belongs_to :billing_address,      class_name: "Address"
       belongs_to :user
       belongs_to :assignee,             class_name: "User"
       belongs_to :stock_location,       class_name: "Location"
