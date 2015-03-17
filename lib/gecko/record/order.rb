452cbf64191226740ad4c2e32aa5b598cbab9075
Bradley Priest <bradleypriest@gmail.com>
2015-03-17 12:35:03 +0800

    Order#tax_override is a BigDecimal

--- lib/gecko/record/order.rb
+++ lib/gecko/record/order.rb
@@ -30,7 +30,7 @@ module Gecko
       attribute :tax_type,              String
       attribute :issued_at,             Date
       attribute :ship_at,               Date
-      attribute :tax_override,          String,     readonly: true
+      attribute :tax_override,          BigDecimal, readonly: true
       attribute :tax_label,             String,     readonly: true
       attribute :source_url,            String
       attribute :document_url,          String,     readonly: true
