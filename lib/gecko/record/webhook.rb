fd1d9f6dfc604b11e3b81718780cec4bf6f8bc94
Bradley Priest <bradleypriest@gmail.com>
2018-09-25 12:33:44 +0800

    Add available webhooks to the model

--- lib/gecko/record/webhook.rb
+++ lib/gecko/record/webhook.rb
@@ -3,6 +3,32 @@ require 'gecko/record/base'
 module Gecko
   module Record
     class Webhook < Base
+      EVENTS = %w[
+        address.create
+        address.update
+        company.create
+        company.update
+        contact.create
+        contact.update
+        fulfillment.create
+        fulfillment.fulfilled
+        fulfillment_return.create
+        image.create
+        invoice.create
+        location.create
+        location.update
+        order.create
+        order.finalized
+        order.fulfilled
+        payment.create
+        procurement.create
+        product.create
+        purchase_order.create
+        stock_adjustment.create
+        stock_transfer.create
+        variant.create
+      ]
+
       attribute :address, String
       attribute :event,   String
     end
