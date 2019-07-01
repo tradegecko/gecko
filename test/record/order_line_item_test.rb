30d78fcc40a4e4d04b6ac1e93009506ace9a85b8
Bradley Priest <bradleypriest@gmail.com>
2019-07-01 11:35:14 +0800

    Add discount_amount and discounted_price

--- test/record/order_line_item_test.rb
+++ test/record/order_line_item_test.rb
@@ -15,4 +15,14 @@ class Gecko::OrderLineItemTest < Minitest::Test
   def test_initializes_record
     assert_instance_of(Gecko::Record::OrderLineItem, @record)
   end
+
+  def test_discounted_price_with_discount
+    record = record_class.new(client, price: 20, discount: 20)
+    assert_equal(16, record.discounted_price)
+  end
+
+  def test_discounted_price_with_discount_amount
+    record = record_class.new(client, price: 20, discount_amount: 5)
+    assert_equal(15, record.discounted_price)
+  end
 end
